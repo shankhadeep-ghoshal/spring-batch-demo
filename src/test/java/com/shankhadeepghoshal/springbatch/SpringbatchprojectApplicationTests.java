7be4c846f0d70fa80ae50905176005a159286a1c
Shankhadeep-Ghoshal <shankhadeepghoshal1996@gmail.com>
2023-02-01 06:58:18 +0200

    Initial commit

---
+++ src/test/java/com/shankhadeepghoshal/springbatch/SpringbatchprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shankhadeepghoshal.springbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbatchprojectApplicationTests {
+
+		@Test
+		void contextLoads() {
+		}
+
+}
